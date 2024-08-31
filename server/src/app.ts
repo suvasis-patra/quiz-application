d19cb2d2d5be33fe5743e7b82e1414d7a05b36ee
suvasis <suvasis02@kgpian.iitkgp.ac.in>
2024-08-31 23:09:37 +0530

    update client resoureces

--- server/src/app.ts
+++ server/src/app.ts
@@ -27,7 +27,7 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/quiz", quizRouter);
 
 app.get("*", (_, res) =>
-  res.sendFile(path.join(__dirname, "./client/dist/index.html"))
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
 );
 
 export { app };
